b407781c260ab8a90998cf51071a16316665ea1b
Virang2991 <46640350+Virang2991@users.noreply.github.com>
2019-04-09 11:55:24 -0400

    Adding Cuisines Model -> Class.

---
+++ FoodOnFinger_API/Models/Cuisine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOnFinger_API.Models
+{
+    public class Cuisine
+    {
+        public int CuisineID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Description { get; set; }
+    }
+}
