fdc9baa856bc752c32128ca12aefbe608623335b
Virang2991 <46640350+Virang2991@users.noreply.github.com>
2019-04-09 11:47:48 -0400

    Configure Database Connection with ProductView Model and ConnectionString.

---
+++ FoodOnFinger_API/Models/ProductView.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOnFinger_API.Models
+{
+    public class ProductView : DbContext
+    {
+        public ProductView(DbContextOptions<ProductView> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Cuisine> Cuisine { get; set; }
+        }
+}
